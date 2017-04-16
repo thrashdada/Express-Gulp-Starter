9b9cd56a70c3c1bbac4d70866aab3ab6ffab7604
thrashdada <amanteadrian@gmail.com>
2017-04-17 07:19:26 +0800

    website stucture update

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About' });
+});
+
 module.exports = router;
